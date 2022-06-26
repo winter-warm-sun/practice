7bdda8efb18fd42ea1ae6a33a1498168e268843d
如风暖阳 <1491788197@qq.com>
2022-06-26 21:08:28 +0800

    。

---
+++ 20220626/src/Demo1.java
@@ -0,0 +1,36 @@
+class Food {
+}
+class Fruit extends Food {
+}
+class Apple extends Fruit {
+}
+class Banana extends Fruit {
+}
+class Message<T> {
+    private T message;
+    public T getMessage() {
+        return message;
+    }
+
+    public void setMessage(T message) {
+        this.message=message;
+    }
+}
+public class Demo1 {
+    public static void main(String[] args) {
+        Message<Fruit> message1=new Message<>();
+        message1.setMessage(new Fruit());
+        fun(message1);
+        Message<Food> message2=new Message<>();
+        message2.setMessage(new Food());
+        fun(message2);
+    }
+    //这里只要是Fruit或者Fruit的父类即可
+    public static void fun(Message<? super Fruit> temp) {
+        //此时可以修改，添加的是Fruit或者Fruit的子类
+        temp.setMessage(new Banana());
+        temp.setMessage(new Apple());
+        //Fruit fruit=temp.getMessage();不能接收，这里无法确定是哪个父类
+        System.out.println(temp.getMessage());//只能直接输出
+    }
+}
---
+++ 20220626/src/TestDemo.java
@@ -0,0 +1,23 @@
+//class Message<T> {
+//    private T message;
+//    public T getMessage() {
+//        return message;
+//    }
+//
+//    public void setMessage(T message) {
+//        this.message=message;
+//    }
+//}
+//public class TestDemo {
+//    public static void main(String[] args) {
+//        Message<String> message1=new Message<>();
+//        message1.setMessage("哈喽啊");
+//        fun(message1);
+//        Message<Integer> message2=new Message<>();
+//        message2.setMessage(10);
+//        fun(message2);
+//    }
+//    public static void fun(Message<?> temp) {
+//        System.out.println(temp.getMessage());
+//    }
+//}
