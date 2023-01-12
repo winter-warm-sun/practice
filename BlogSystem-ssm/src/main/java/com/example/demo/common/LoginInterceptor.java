1fab742bbbe550837505d077f730ba02f327280b
如风暖阳 <1491788197@qq.com>
2023-01-12 08:44:53 +0800

    。

---
+++ BlogSystem-ssm/src/main/java/com/example/demo/common/LoginInterceptor.java
@@ -0,0 +1,27 @@
+package com.example.demo.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * 登录拦截器
+ */
+@Component
+@Slf4j
+public class LoginInterceptor implements HandlerInterceptor {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        // 登录判断业务
+        HttpSession session=request.getSession(false);
+        if(session!=null&&session.getAttribute("userinfo")!=null) {
+            return true;
+        }
+        log.error("当前用户没有访问权限");
+        response.setStatus(401);
+        return false;
+    }
+}
