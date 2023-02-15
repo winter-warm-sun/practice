a0ad1b5e15fa4da526e41b64a844da082a722fcb
如风暖阳 <1491788197@qq.com>
2023-02-15 16:00:55 +0800

    。

--- 20230215/src/test/java/Application.java
+++ 20230215/src/test/java/Application.java
@@ -1,6 +1,8 @@
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
         AutoTest autoTest=new AutoTest();
-        autoTest.windowControl();
+        autoTest.picture();
     }
 }
--- 20230215/src/test/java/AutoTest.java
+++ 20230215/src/test/java/AutoTest.java
@@ -1,6 +1,12 @@
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Set;
 
 public class AutoTest {
     ChromeDriver driver=new ChromeDriver();
@@ -40,6 +46,37 @@ public class AutoTest {
         Thread.sleep(3000);
         driver.manage().window().setSize(new Dimension(1024,888));
         Thread.sleep(2000);
+        driver.quit();
+    }
+
+    public void switchTest() throws InterruptedException {
+        driver.get("https://www.baidu.com");
+        driver.findElement(By.cssSelector("#s-top-left > a:nth-child(6)")).click();
+        // 获取当前页面的句柄
+        String curHandle=driver.getWindowHandle();
+        System.out.println("当前页面的句柄："+curHandle);
+        // 先获取所有标签的句柄
+        Set<String> handles=driver.getWindowHandles();
+        for(String handle:handles) {
+            if(handle!=curHandle) {
+                driver.switchTo().window(handle);
+            }
+        }
+        driver.findElement(By.cssSelector("#homeSearchForm > span.s_btn_wr > input"));
+        driver.quit();
+    }
+
+    public void picture() throws InterruptedException, IOException {
+        driver.get("https://www.baidu.com");
+        driver.findElement(By.cssSelector("#kw")).sendKeys("迪丽热巴");
+        driver.findElement(By.cssSelector("#su")).click();
+        Thread.sleep(3000);
+        // 屏幕截图（保存现场）
+        File srcFile=driver.getScreenshotAs(OutputType.FILE);
+        // 把屏幕截好的文件放到指定的路径下
+        String fileName="my.png";
+        FileUtils.copyFile(srcFile,new File(fileName));
+        driver.findElement(By.cssSelector("#\\31  > div > div > div > div > div.cos-row.row-text_Johh7.row_5y9Az > div > a > div > p > span > span"));
         driver.quit();
     }
 }
