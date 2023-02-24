235d972dde44e4362e494da5dc30214f400df1af
如风暖阳 <1491788197@qq.com>
2023-02-24 09:27:50 +0800

    。

---
+++ 20230224/src/Solution.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+class Solution {
+    public static int search(int[] nums, int target) {
+        int count=0;
+        for(int i=0;i<nums.length;i++) {
+            if(nums[i]==target) {
+                while(i<nums.length&&nums[i]==target) {
+                    count++;
+                    i++;
+                }
+                return count;
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] arr=new int[]{1};
+        System.out.println(search(arr, 1));
+    }
+}
